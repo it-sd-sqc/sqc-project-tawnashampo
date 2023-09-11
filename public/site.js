12e8255c862aba139ade0b050062a43b48702e4d
Tawna Shampo <tawnashampo@gmail.com>
2023-09-10 22:11:33 -0500

    Website changes

--- public/site.js
+++ public/site.js
@@ -1,11 +1,39 @@
-const darkModeToggle = document.getElementById("darkModeToggle");
-const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownToggle = document.getElementById("navigation-dropdown");
+    const dropdownContent = document.querySelector(".dropdown-content");
 
-function toggleDarkMode () {
-    document.body.classList.toggle("dark-mode");
+    dropdownToggle.addEventListener("click", function () {
+        dropdownContent.classList.toggle("show");
+    });
 
-    const isDarkMode = body.classList.contains("dark-mode");
-    darkModeToggle.textContent = isDarkMode ? "☀️" : "🌙";
-}
+    window.addEventListener("click", function (event) {
+        if (!event.target.matches("#navigation-dropdown")) {
+            if (dropdownContent.classList.contains("show")) {
+                dropdownContent.classList.remove("show");
+            }
+        }
+    });
+});
 
-darkModeToggle.addEventListener("click", toggleDarkMode);
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const mainBody = document.getElementById("mainBody");
+
+    function toggleDarkMode() {
+        mainBody.classList.toggle("dark-mode");
+        darkModeToggle.innerHTML = mainBody.classList.contains("dark-mode") ? "☀️" : "🌙";
+        const isDarkModeEnabled = mainBody.classList.contains("dark-mode");
+        localStorage.setItem("darkModeEnabled", isDarkModeEnabled);
+    }
+
+    const isDarkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
+    if (isDarkModeEnabled) {
+        mainBody.classList.add("dark-mode");
+        darkModeToggle.innerHTML = "☀️";
+    }
+
+    darkModeToggle.addEventListener("click", toggleDarkMode);
+});
+
+
+
