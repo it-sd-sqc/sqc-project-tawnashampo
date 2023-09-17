0fb46b520ed64a5136a65fd04421707878775976
Tawna Shampo <tawnashampo@gmail.com>
2023-09-16 22:18:02 -0500

    book data added

--- public/site.js
+++ public/site.js
@@ -1,39 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownToggle = document.getElementById("navigation-dropdown");
-    const dropdownContent = document.querySelector(".dropdown-content");
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownToggle = document.getElementById('navigation-dropdown')
+    const dropdownContent = document.querySelector('.dropdown-content')
 
-    dropdownToggle.addEventListener("click", function () {
-        dropdownContent.classList.toggle("show");
-    });
+    dropdownToggle.addEventListener('click', function () {
+        dropdownContent.classList.toggle('show')
+    })
 
-    window.addEventListener("click", function (event) {
-        if (!event.target.matches("#navigation-dropdown")) {
-            if (dropdownContent.classList.contains("show")) {
-                dropdownContent.classList.remove("show");
+    window.addEventListener('click', function (event) {
+        if (!event.target.matches('#navigation-dropdown')) {
+            if (dropdownContent.classList.contains('show')) {
+                dropdownContent.classList.remove('show')
             }
         }
-    });
-});
+    })
+})
 
-document.addEventListener("DOMContentLoaded", function () {
-    const darkModeToggle = document.getElementById("darkModeToggle");
-    const mainBody = document.getElementById("mainBody");
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('darkModeToggle')
+    const mainBody = document.getElementById('mainBody')
 
-    function toggleDarkMode() {
-        mainBody.classList.toggle("dark-mode");
-        darkModeToggle.innerHTML = mainBody.classList.contains("dark-mode") ? "☀️" : "🌙";
-        const isDarkModeEnabled = mainBody.classList.contains("dark-mode");
-        localStorage.setItem("darkModeEnabled", isDarkModeEnabled);
+    function toggleDarkMode () {
+        mainBody.classList.toggle('dark-mode')
+        darkModeToggle.innerHTML = mainBody.classList.contains('dark-mode') ? '☀️' : '🌙'
+        const isDarkModeEnabled = mainBody.classList.contains('dark-mode')
+        localStorage.setItem('darkModeEnabled', isDarkModeEnabled)
     }
 
-    const isDarkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
+    const isDarkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true'
     if (isDarkModeEnabled) {
-        mainBody.classList.add("dark-mode");
-        darkModeToggle.innerHTML = "☀️";
+        mainBody.classList.add('dark-mode')
+        darkModeToggle.innerHTML = '☀️'
     }
 
-    darkModeToggle.addEventListener("click", toggleDarkMode);
-});
-
-
-
+    darkModeToggle.addEventListener('click', toggleDarkMode)
+})
